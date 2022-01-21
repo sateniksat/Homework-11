7a1c519219b4cb540318010147ce6ac575b3f1c1
Satenik Shahi <shahijanisaten@gmail.com>
2022-01-21 20:32:30 +0330

    changes

--- Satenik_Shahi_Jani_hw11_maktab66/hw1/1/assets/JS/main.js
+++ Satenik_Shahi_Jani_hw11_maktab66/hw1/1/assets/JS/main.js
@@ -8,59 +8,83 @@
 // const drawMessage = () => `It's a Draw!`;
 const input = document.getElementById("myInput");
 const todoList = document.querySelector(".todo-list");
+let value1 = document.createTextNode(input.value);
 
+
+//add itm
 input.addEventListener("keypress", function (e) {
   if (e.key === "Enter" && input.value === "") {
     alert("You must write something!");
   }
   if (e.key === "Enter" && input.value !== "") {
     let todoLI = document.createElement("li");
-    let div1=document.createElement("div");
-    
-    // todoLI.classList.add("uncomplited");
-    todoLI.innerHTML = `<div class="mark uncompleted"><div class="tik"></div></div>${input.value}<span class="close"></span>`;
+    todoLI.classList.add("uncomplited");    
+    todoLI.innerHTML = `<div class="mark uncompleted"><div class="tik"></div></div>${input.value} <span class="close"></span>`;
     todoList.appendChild(todoLI);
     // result.innerHTML = addtodoli();
     input.value = "";
   }
 });
-
+//clear
 document.querySelector(".clear").addEventListener("click", () => {
   document.querySelectorAll("li.completed").forEach((element) => {
     element.closest("li").remove();
   });
 });
-
+//mark
 document.querySelectorAll(".mark").forEach((box) =>
   box.addEventListener("click", () => {
     box.classList.toggle("completed");
+    box.classList.toggle("uncompleted");
     box.querySelector(".tik").classList.toggle("completed");
     box.closest("li").classList.toggle("completed");
+    box.closest("li").classList.toggle("uncompleted");
   })
 );
-// document.querySelectorAll(".mark.uncompleted").forEach((box) =>
-//   box.addEventListener("click", () => {
-//     box.classList.replace("uncompleted", "completed");
-//     box.querySelector(".tik").classList.add("completed");
-//     box.closest("li").classList.add("completed");
-//   })
-// );
-// document.querySelectorAll('.mark').forEach(element => {
-//     element.addEventListener('click', (evt) => {
-//         evt.target.classList.add("complited");
+//close
+document.querySelectorAll(".close").forEach((box) =>
+  box.addEventListener("click", () => {
+    box.closest("li").remove();
+  })
+);
+function colorChange() {
+  document.querySelectorAll(".btn").forEach((element) => {
+    if (element.classList.contains("blue")) {
+      element.classList.remove("blue");
+    }
+  });
+}
+//all
+document.querySelector(".all").addEventListener("click", () => {
+  colorChange();
+  document.querySelector(".all").classList.add("blue");
+  document.querySelectorAll("li").forEach((element) => {
+    element.classList.remove("display-none");
+  });
+});
+//active
+document.querySelector(".active").addEventListener("click", () => {
+  colorChange();
+  document.querySelector(".active").classList.add("blue");
 
-// });
-// // });
-// // });
-// // .addEventListener('click', (evt) => {
-// //         evt.target.classList.add("complited");
-
-// // });
-// document.querySelector('.close').addEventListener('click', () => {
-//     document.querySelectorAll('li.completed').forEach(item => {
-//         (item.closest('li').remove());
-//     }});
-// });
+  document.querySelectorAll("li.completed").forEach((element) => {
+    element.closest("li").classList.add("display-none");
+  });
+  document.querySelectorAll("li.uncompleted").forEach((element) => {
+    element.closest("li").classList.remove("display-none");
+  });
+});
+//completed
+document.querySelector(".complete").addEventListener("click", () => {
+  colorChange();
+  document.querySelector(".complete").classList.add("blue");
+  document.querySelectorAll("li.completed").forEach((element) => {
+    element.closest("li").classList.remove("display-none");
+  });
+  document.querySelectorAll("li.uncompleted").forEach((element) => {
+    element.closest("li").classList.add("display-none");
+  });
+});
 
 //style
 document.querySelector(".mode.moon").addEventListener("click", () => {
